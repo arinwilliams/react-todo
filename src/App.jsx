e102eff5210deb65cda74d82f67dc74e3b59911e
Arin Williams <aw@arinwilliams.com>
2025-12-24 11:41:26 -0800

    convert to controlled component with checkboxes and no empty items allowed and conditional message

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import { useState } from 'react';
-import './App.css';
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
-
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+
   const addTodo = (title) => {
-    const newTodo = { title, id: Date.now() };
-    setTodoList((todoList) => [...todoList, newTodo]);
-    }
-  
-    return (
+    const newTodo = {
+      title,
+      id: Date.now(),
+      isCompleted: false,   
+    };
+    setTodoList((prevList) => [...prevList, newTodo]);
+  };
+
+  const completeTodo = (id) => {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
+  };
+
+  return (
     <>
       <div>
-      <h1>Todo List</h1>
-      <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList}/>
+        <h1>Todo List</h1>
+        <TodoForm onAddTodo={addTodo} />
+        <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
       </div>
     </>
   );
